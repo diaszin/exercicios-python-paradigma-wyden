e9287812ba5f45f0833e56a7ddafd80c6fdd659a
Kaique Dias <kaique.dias@ssig.tech>
2023-11-07 12:04:24 -0300

    ADD ATIVIDADES 2

--- Lista de Exercícios de Repetição USP/Lista de Exercícios de Algoritmos Repetição e Vetores PUCRS/questao1.py
+++
@@ -1,9 +0,0 @@
-qntd_numeros_negativos = 0
-
-for ind in range(5):
-    num = int(input("Digite o número: "))
-    if num < 0:
-        qntd_numeros_negativos +=1
-
-
-print(f"Nessa sequência de números tem {qntd_numeros_negativos} numeros negativos")
